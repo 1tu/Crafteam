1442f4c0e2a18e86b938292107ca7ecc0c2ea4bd
Skripko Andrey <itu.ccb@gmail.com>
2018-03-20 20:11:17 +0300

    sample

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <template lang='pug'>
   section.test {{ todayName }}
-    Loading(:active='true')
+    Loading(active)
+    Menu(:linkList='menu' @zzk='log' :test='ss')
 </template>
 
 <script lang='ts'>
@@ -8,12 +9,29 @@ import Component from 'nuxt-class-component';
 import Vue from 'vue';
 
 import Loading from '../components/Loading.vue';
-import { RootGetter } from '../store/root.store';
+import Menu from '../components/Menu.vue';
+import { mainMenu } from '../shared/menu.variables';
+import { RootGetter, RootState, RootMutation, RootMutation } from '../store/root.store';
+import { setTimeout } from 'timers';
 
-@Component({ components: { Loading } })
+@Component({ components: { Loading, Menu } })
 export default class extends Vue {
+  public active = true;
   @RootGetter todayName;
+  @RootMutation setAppName;
 
-  public created(): void {}
+  public menu = mainMenu;
+  public ss = 'fsafafsfasfsf';
+
+  public log(data) {
+    console.log('data', data, data.link);
+  }
+
+  public created(): void {
+    setTimeout(() => {
+      this.menu = [];
+      this.active = false;
+    }, 2000);
+  }
 }
 </script>
